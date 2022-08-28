34ed1e38b55f67de12e0125c18af2b465ca8e4a4
Lencione <wesley@lencione.com.br>
2022-08-28 12:04:12 -0300

    RenderCandidates
    Renderizando candidatos no topo do sistema

--- urna.cpp
+++ urna.cpp
@@ -3,13 +3,19 @@
 #include <conio.h>
 #include <string.h>
 
+// CONSTANTES - TAMANHO DE TELA
+#define COLS 140
+#define ROWS 40
+
 // STRUCTS
-typedef struct{
+typedef struct
+{
     int number;
     char *name;
 } Person;
 
-typedef struct{
+typedef struct
+{
     int number;
     char *name;
     Person president;
@@ -19,7 +25,8 @@ typedef struct{
     Person state;
 } Entourage;
 
-typedef struct{
+typedef struct
+{
     int ra;
     int president;
     int governor;
@@ -28,67 +35,86 @@ typedef struct{
     int state;
 } Voter;
 
-
-// CONSTANTES - TAMANHO DE TELA
-#define COLS 140
-#define ROWS 40
-
 // FUNCOES
 void gotoxy(int x, int y);
 void renderBorder(int x, int y, int cols, int rows);
 void renderButton(int x, int y, char *text);
 void createButtons(int x, int y);
 void renderLayout();
-Entourage* createCandidates();
+Entourage *createCandidates(Entourage *entourages);
+void resetCandidateList();
+void renderCandidateList(Entourage *candidates, char *type);
 
-int main(){
+int main()
+{
 
-    Entourage *entourages = createCandidates();
+    Entourage ent[4];
+    Entourage *candidates = createCandidates(ent);
 
+    
     system("color F0");
     system("mode con:cols=140 lines=45");
-    
-    //renderLayout();
-    
-    gotoxy(1,42);
 
-    printf("%s", entourages[0].president.name);
+    renderLayout();
+    renderCandidateList(candidates, "president");
+
+    renderBorder(3,15,4,4);
+    renderBorder(7,15,4,4);
+    renderBorder(11,15,4,4);
+
+
+    gotoxy(1, 42);
 
     return 0;
 }
 
 void gotoxy(int x, int y)
 {
-  COORD coord;
-  coord.X = x;
-  coord.Y = y;
-  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-void renderBorder(int x, int y, int rows, int cols){
-    int finalx = x + cols-1;
-    int finaly = y + rows-1;
+void renderBorder(int x, int y, int rows, int cols)
+{
+    int finalx = x + cols - 1;
+    int finaly = y + rows - 1;
 
-    for(int i = x; i <= finalx; i++){
-        for(int j = y; j<= finaly; j++){
-            if(i == x || j == y || i == finalx || j == finaly){
+    for (int i = x; i <= finalx; i++)
+    {
+        for (int j = y; j <= finaly; j++)
+        {
+            if (i == x || j == y || i == finalx || j == finaly)
+            {
                 gotoxy(i, j);
-                if(i == x && j == y){
-                    //Canto superior esquerdo
+                if (i == x && j == y)
+                {
+                    // Canto superior esquerdo
                     printf("%c", 201);
-                }else if(i == x && j == finaly){
+                }
+                else if (i == x && j == finaly)
+                {
                     // Canto inferior esquerdo
-	                printf("%c", 200);
-                }else if(i == finalx && j == y){
-                     // Canto superior direito
+                    printf("%c", 200);
+                }
+                else if (i == finalx && j == y)
+                {
+                    // Canto superior direito
                     printf("%c", 187);
-                }else if(i == finalx && j == finaly){
+                }
+                else if (i == finalx && j == finaly)
+                {
                     // Canto inferior direito
                     printf("%c", 188);
-                }else if(i == x || i == finalx){
+                }
+                else if (i == x || i == finalx)
+                {
                     // Linha vertical
-                    printf("%c", 186);	
-                }else{
+                    printf("%c", 186);
+                }
+                else
+                {
                     // Linha horizontal
                     printf("%c", 205);
                 }
@@ -97,63 +123,61 @@ void renderBorder(int x, int y, int rows, int cols){
     }
 }
 
-void renderButton(int x, int y, char *text){
-    gotoxy(x+3,y+2);
+void renderButton(int x, int y, char *text)
+{
+    gotoxy(x + 3, y + 2);
     printf("%s", text);
-    renderBorder(x,y, 5,strlen(text)+6);
+    renderBorder(x, y, 5, strlen(text) + 6);
 }
-void createButtons(int x, int y){
+void createButtons(int x, int y)
+{
     char *numbers[10] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
-    
+
     // Linhas
-    for(int i = 0; i < 3; i++){
+    for (int i = 0; i < 3; i++)
+    {
         // Colunas
-        for(int j = 0; j < 3; j++){
+        for (int j = 0; j < 3; j++)
+        {
             // Posição x + 8*j (Somar 8 a cada iteração do for) -> espaçamento horizontal dos botoes
             // Posição y + 5*i (Somar 5 a cada iteração do for) -> espaçamento vertical dos botoes
             // numbers[(i*3)+j] -> Somando um a cada iteração do for, ex: (0 * 3) + 0 = 0, (0 * 3) + 1 = 1, (0 * 3) + 2 = 2, (1 * 3) + 0 = 3, ...
-            renderButton(x + (8*j), y + (5*i), numbers[(i*3)+j]);
+            renderButton(x + (8 * j), y + (5 * i), numbers[(i * 3) + j]);
         }
     }
 
     // Criar o botão de número "0"
-    renderButton(x + (8*1), y + (5*3), numbers[9]);
-    renderButton(x + (8*0)-7, y + (5*4), "BRANCO");
-    renderButton(x + (8*1)-3, y + (5*4), "CORRIGE");
-    renderButton(x + (8*2)+2, y + (5*4), "CONFIRMA");
+    renderButton(x + (8 * 1), y + (5 * 3), numbers[9]);
+    renderButton(x + (8 * 0) - 7, y + (5 * 4), "BRANCO");
+    renderButton(x + (8 * 1) - 3, y + (5 * 4), "CORRIGE");
+    renderButton(x + (8 * 2) + 2, y + (5 * 4), "CONFIRMA");
 }
 
-void renderLayout(){
-    //Criar borda total
+void renderLayout()
+{
+    // Criar borda total
     renderBorder(0, 0, ROWS, COLS);
 
     // Criar borda da área de voto
     renderBorder(1, 7, 32, 85);
-    
+
     // Criar borda dos candidatos
     renderBorder(1, 1, 6, 138);
 
     // Criar borda da área de digitos
     renderBorder(86, 7, 32, 53);
 
-    // Criar borda JUSTICA ELEITORAL    
+    // Criar borda JUSTICA ELEITORAL
     renderBorder(87, 8, 3, 51);
-    gotoxy(103,9);
+    gotoxy(103, 9);
     printf("JUSTICA ELEITORAL");
-    
+
     // Criar botoes
-    createButtons(100,12);
+    createButtons(100, 12);
 }
 
-//d estadual - 5
-//d federal - 4
-//senador - 3
-//gov - 2
-//pres - 2
-
-Entourage* createCandidates()
+Entourage *createCandidates(Entourage *entourages)
 {
-    Entourage entourages[4];
     entourages[0].number = 11;
     entourages[0].name = "AA";
     entourages[0].president.number = 11;
@@ -189,12 +213,12 @@ Entourage* createCandidates()
     entourages[2].senator.number = 333;
     entourages[2].senator.name = "Caio Costa";
     entourages[2].federal.number = 3333;
-    entourages[2].federal.name ="Gabrielly Jesus";
+    entourages[2].federal.name = "Gabrielly Jesus";
     entourages[2].state.number = 33333;
     entourages[2].state.name = "Maria Alice Fernandes";
 
     entourages[3].number = 44;
-    entourages[3].name = "DD"; 
+    entourages[3].name = "DD";
     entourages[3].president.number = 44;
     entourages[3].president.name = "Thales Barros";
     entourages[3].governor.number = 44;
@@ -206,8 +230,76 @@ Entourage* createCandidates()
     entourages[3].state.number = 44444;
     entourages[3].state.name = "Isabelly Pires";
 
-    printf("%s", entourages[3].president.name);
-
     return entourages;
 }
 
+void resetCandidateList(){
+    for(int i = 2; i < 138; i++){
+        for(int j = 2; j < 5; j++){
+            gotoxy(i,j);
+            printf(" ");
+        }
+    }
+}
+
+void renderCandidateList(Entourage *candidates, char *type)
+{
+    resetCandidateList();
+    gotoxy(60, 2);
+
+    if (strcmp(type, "president") == 0)
+    {
+        printf("CANDIDATOS A PRESIDENTE");
+        for (int i = 0; i < 4; i++)
+        {
+            gotoxy(2 + (i * 30), 4);
+            printf("%s", candidates[i].president.name);
+            gotoxy(2 + (i * 30), 5);
+            printf("Numero: %d", candidates[i].president.number);
+        }
+    }
+    else if (strcmp(type, "governor") == 0)
+    {
+        printf("CANDIDATOS A GOVERNADOR");
+        for (int i = 0; i < 4; i++)
+        {
+            gotoxy(2 + (i * 30), 4);
+            printf("%s", candidates[i].governor.name);
+            gotoxy(2 + (i * 30), 5);
+            printf("Numero: %d", candidates[i].governor.number);
+        }
+    }
+    else if (strcmp(type, "state") == 0)
+    {
+        printf("CANDIDATOS A DEPUTADO ESTADUAL");
+        for (int i = 0; i < 4; i++)
+        {
+            gotoxy(2 + (i * 30), 4);
+            printf("%s", candidates[i].state.name);
+            gotoxy(2 + (i * 30), 5);
+            printf("Numero: %d", candidates[i].state.number);
+        }
+    }
+    else if (strcmp(type, "senator") == 0)
+    {
+        printf("CANDIDATOS A SENADOR");
+        for (int i = 0; i < 4; i++)
+        {
+            gotoxy(2 + (i * 30), 4);
+            printf("%s", candidates[i].senator.name);
+            gotoxy(2 + (i * 30), 5);
+            printf("Numero: %d", candidates[i].senator.number);
+        }
+    }
+    else
+    {
+        printf("CANDIDATOS A DEPUTADO ESTADUAL");
+        for (int i = 0; i < 4; i++)
+        {
+            gotoxy(2 + (i * 30), 4);
+            printf("%s", candidates[i].federal.name);
+            gotoxy(2 + (i * 30), 5);
+            printf("Numero: %d", candidates[i].federal.number);
+        }
+    }
+}
